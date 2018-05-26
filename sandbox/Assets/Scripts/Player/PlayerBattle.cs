ef37fb59e3ef28d963d6f7d2cf5daafc27c4588e
Yugo <yugobrunet@gmail.com>
2018-05-26 13:47:07 -0400

    Added new scene for instanced fighting. Added hurtbox on enemy, floating hitbox for player. Currently there is no combat.

---
+++ sandbox/Assets/Scripts/Player/PlayerBattle.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerBattle : MonoBehaviour
+{
+    private CharacterController controller;
+
+    public Collider[] attackHitBoxes;
+
+	// Use this for initialization
+	void Start ()
+    {
+        controller = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    private void LaunchAttack(Collider col)
+    {
+
+    }
+}
