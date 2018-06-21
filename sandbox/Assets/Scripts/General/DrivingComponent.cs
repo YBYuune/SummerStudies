a3b55647b89a59c56f126cb6579f95e53a5701cf
Yugo <yugobrunet@gmail.com>
2018-06-21 18:27:04 -0400

    Mounting is a thing

---
+++ sandbox/Assets/Scripts/General/DrivingComponent.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrivingComponent : MonoBehaviour, IInteractableComp {
+
+    public Transform mountLocation = null;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    public void Interact(GameObject interactingObject)
+    {
+        PlayerInteract playerInteractComp = interactingObject.GetComponent<PlayerInteract>();
+
+        if (playerInteractComp)
+        {
+            playerInteractComp.DriveVehicle(this);
+        }
+    }
+}
